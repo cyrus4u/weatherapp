4d34ebf0f721bb57fa71cc2d4655dc856b10ef23
cyrus4u <cyrus4u@users.noreply.github.com>
2025-11-20 16:44:37 +0330

    adding Text Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:weatherapp/theme/text_theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,133 +19,257 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(textTheme: AppTextThemes.textTheme16),
+
       home: Scaffold(
         backgroundColor: Colors.black,
         appBar: AppBar(title: Text('Weather App'), centerTitle: true),
         body: Center(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 50.0),
-                child: Text(
-                  'Mountain View',
-                  style: TextStyle(color: Colors.white, fontSize: 35),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: Text(
-                  'Clear Sky',
-                  style: TextStyle(color: Colors.grey, fontSize: 20),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: Icon(
-                  Icons.wb_sunny_outlined,
-                  size: 80,
-                  color: Colors.white,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 20),
-                child: Text(
-                  '14°',
-                  style: TextStyle(fontSize: 60, color: Colors.white),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Column(
-                    children: [
-                      Text(
-                        'Max',
-                        style: TextStyle(color: Colors.grey, fontSize: 16),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        '16°',
-                        style: TextStyle(color: Colors.white, fontSize: 16),
-                      ),
-                    ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 50.0),
+                  child: Text(
+                    'Mountain View',
+                    style: TextStyle(color: Colors.white, fontSize: 35),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Container(width: 1, height: 40, color: Colors.white),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: Text(
+                    'Clear Sky',
+                    style: TextStyle(color: Colors.grey, fontSize: 20),
                   ),
-                  Column(
-                    children: [
-                      Text(
-                        'min',
-                        style: TextStyle(color: Colors.grey, fontSize: 16),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        '10°',
-                        style: TextStyle(color: Colors.white, fontSize: 16),
-                      ),
-                    ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: Icon(
+                    Icons.wb_sunny_outlined,
+                    size: 80,
+                    color: Colors.white,
                   ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10.0),
-                child: Container(
-                  color: Colors.grey,
-                  height: 1,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Text(
+                    '14°',
+                    style: TextStyle(fontSize: 60, color: Colors.white),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          'Max',
+                          style: Theme.of(
+                            context,
+                          ).textTheme.titleMedium!.copyWith(color: Colors.grey),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          '16°',
+                          style: Theme.of(
+                            context,
+                          ).textTheme.titleMedium!.copyWith(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Container(
+                        width: 1,
+                        height: 40,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'min',
+                          style: Theme.of(
+                            context,
+                          ).textTheme.titleMedium!.copyWith(color: Colors.grey),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          '10°',
+                          style: Theme.of(
+                            context,
+                          ).textTheme.titleMedium!.copyWith(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Container(
+                    color: Colors.grey,
+                    height: 1,
+                    width: double.infinity,
+                  ),
+                ),
+                Container(
                   width: double.infinity,
-                ),
-              ),
-              Container(
-                width: double.infinity,
-                height: 100,
-                child: Center(
-                  child: ScrollConfiguration(
-                    behavior: ScrollConfiguration.of(context).copyWith(
-                      dragDevices: {
-                        PointerDeviceKind.mouse,
-                        PointerDeviceKind.touch,
-                        PointerDeviceKind.trackpad,
-                      },
-                    ),
-                    child: ListView.builder(
-                      itemCount: 10,
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          height: 50,
-                          width: 70,
-                          child: Card(
-                            color: Colors.transparent,
-                            child: Column(
-                              children: [
-                                Text(
-                                  'Fri, 8pm',
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    color: Colors.grey,
+                  height: 80,
+                  child: Center(
+                    child: ScrollConfiguration(
+                      behavior: ScrollConfiguration.of(context).copyWith(
+                        dragDevices: {
+                          PointerDeviceKind.mouse,
+                          PointerDeviceKind.touch,
+                          PointerDeviceKind.trackpad,
+                        },
+                      ),
+                      child: ListView.builder(
+                        itemCount: 6,
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            height: 50,
+                            width: 70,
+                            child: Card(
+                              color: Colors.transparent,
+                              child: Column(
+                                children: [
+                                  Text(
+                                    'Fri, 8pm',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .titleMedium!
+                                        .copyWith(
+                                          color: Colors.grey,
+                                          fontSize: 14,
+                                        ),
                                   ),
-                                ),
-                                Icon(Icons.cloud, color: Colors.white),
-                                Text(
-                                  '14°',
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    color: Colors.grey,
+                                  Icon(Icons.cloud, color: Colors.white),
+                                  Text(
+                                    '14°',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .titleMedium!
+                                        .copyWith(
+                                          color: Colors.grey,
+                                          fontSize: 14,
+                                        ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
-                        );
-                      },
+                          );
+                        },
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Container(
+                    color: Colors.grey,
+                    height: 1,
+                    width: double.infinity,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          'Wind Speed',
+                          style: Theme.of(
+                            context,
+                          ).textTheme.titleMedium!.copyWith(color: Colors.grey),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          '73 m/s',
+
+                          style: Theme.of(context).textTheme.titleMedium!
+                              .copyWith(color: Colors.white, fontSize: 14),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                      child: Container(
+                        width: 1,
+                        height: 40,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'Sunrise',
+                          style: Theme.of(
+                            context,
+                          ).textTheme.titleMedium!.copyWith(color: Colors.grey),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          '6:20 AM',
+                          style: Theme.of(context).textTheme.titleMedium!
+                              .copyWith(color: Colors.white, fontSize: 14),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                      child: Container(
+                        width: 1,
+                        height: 40,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'Sunset',
+                          style: Theme.of(
+                            context,
+                          ).textTheme.titleMedium!.copyWith(color: Colors.grey),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          '6:30 PM',
+                          style: Theme.of(context).textTheme.titleMedium!
+                              .copyWith(color: Colors.white, fontSize: 14),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                      child: Container(
+                        width: 1,
+                        height: 40,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'Humidity',
+                          style: Theme.of(
+                            context,
+                          ).textTheme.titleMedium!.copyWith(color: Colors.grey),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          '73%',
+                          style: Theme.of(context).textTheme.titleMedium!
+                              .copyWith(color: Colors.white, fontSize: 14),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
---
+++ lib/theme/text_theme.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+
+class AppTextThemes {
+  static const TextTheme textTheme14 = TextTheme(
+    displayLarge: TextStyle(fontSize: 14, color: Colors.white),
+    displayMedium: TextStyle(fontSize: 14, color: Colors.white),
+    displaySmall: TextStyle(fontSize: 14, color: Colors.white),
+
+    headlineLarge: TextStyle(fontSize: 14, color: Colors.white),
+    headlineMedium: TextStyle(fontSize: 14, color: Colors.white),
+    headlineSmall: TextStyle(fontSize: 14, color: Colors.white),
+
+    titleLarge: TextStyle(fontSize: 14, color: Colors.white),
+    titleMedium: TextStyle(fontSize: 14, color: Colors.white),
+    titleSmall: TextStyle(fontSize: 14, color: Colors.white),
+
+    bodyLarge: TextStyle(fontSize: 14, color: Colors.white),
+    bodyMedium: TextStyle(fontSize: 14, color: Colors.white),
+    bodySmall: TextStyle(fontSize: 14, color: Colors.white),
+
+    labelLarge: TextStyle(fontSize: 14, color: Colors.white),
+    labelMedium: TextStyle(fontSize: 14, color: Colors.white),
+    labelSmall: TextStyle(fontSize: 14, color: Colors.white),
+  );
+
+  static const TextTheme textTheme16 = TextTheme(
+    displayLarge: TextStyle(fontSize: 16, color: Colors.white),
+    displayMedium: TextStyle(fontSize: 16, color: Colors.white),
+    displaySmall: TextStyle(fontSize: 16, color: Colors.white),
+
+    headlineLarge: TextStyle(fontSize: 16, color: Colors.white),
+    headlineMedium: TextStyle(fontSize: 16, color: Colors.white),
+    headlineSmall: TextStyle(fontSize: 16, color: Colors.white),
+
+    titleLarge: TextStyle(fontSize: 16, color: Colors.white),
+    titleMedium: TextStyle(fontSize: 16, color: Colors.white),
+    titleSmall: TextStyle(fontSize: 16, color: Colors.white),
+
+    bodyLarge: TextStyle(fontSize: 16, color: Colors.white),
+    bodyMedium: TextStyle(fontSize: 16, color: Colors.white),
+    bodySmall: TextStyle(fontSize: 16, color: Colors.white),
+
+    labelLarge: TextStyle(fontSize: 16, color: Colors.white),
+    labelMedium: TextStyle(fontSize: 16, color: Colors.white),
+    labelSmall: TextStyle(fontSize: 16, color: Colors.white),
+  );
+}
