35a6453bfebbba21194653fc88cae564fd860b77
cyrus4u <cyrus4u@users.noreply.github.com>
2025-11-20 11:40:44 +0330

    UI

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,80 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Container(color: Colors.red),
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(title: Text('Weather App'), centerTitle: true),
+        body: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 50.0),
+                child: Text(
+                  'Mountain View',
+                  style: TextStyle(color: Colors.white, fontSize: 35),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: Text(
+                  'Clear Sky',
+                  style: TextStyle(color: Colors.grey, fontSize: 20),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: Icon(
+                  Icons.wb_sunny_outlined,
+                  size: 80,
+                  color: Colors.white,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  '14°',
+                  style: TextStyle(fontSize: 60, color: Colors.white),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      Text(
+                        'Max',
+                        style: TextStyle(color: Colors.grey, fontSize: 16),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        '16°',
+                        style: TextStyle(color: Colors.grey, fontSize: 16),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Container(width: 1, height: 40, color: Colors.white),
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        'min',
+                        style: TextStyle(color: Colors.grey, fontSize: 16),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        '10°',
+                        style: TextStyle(color: Colors.grey, fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
